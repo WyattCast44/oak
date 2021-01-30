23fae06568aab7a5da5e4a9af708dcf3eefd7050
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-01-29 19:21:54 -0800

    wip

--- app.py
+++ app.py
@@ -1,28 +1,75 @@
+import abc
+from oak.Support import CommandRegistrar
 from oak import Application, ContainerTwo
 from oak.Options import HelpOption, SilentOption
 from oak.Commands import RunCommand, HelpCommand, ListCommand, VersionCommand
-from oak.Support import CommandRegistrar
+
+
+class MailInterface:
+
+    pass
+
+
+class ZeroConfig:
+
+    def __init__(self):
+        pass
 
 
 class SMTPMailer:
 
-    def __init__(self, message='default'):
+    def __init__(self, message):
 
         print(message)
 
 
-class MailServiceProvider:
+class SMTPMailerProvider:
 
-    def register(self):
-        return SMTPMailer('provided via ioc')
+    def __new__(cls):
+        return SMTPMailer("Tada")
 
 
 c = ContainerTwo()
-# c.bind(SMTPMailer, SMTPMailer)
-# c.bind(SMTPMailer, MailServiceProvider, 'register')
-m = c.make(SMTPMailer)
 
+# # we can provide the container instructions on how to init a class
+# c.bind(SMTPMailer, SMTPMailerProvider)
+# m = c.make(SMTPMailer)
+# print(isinstance(m, SMTPMailer))
+# quit()
+
+# # we can use a string as a abstract key
+# c.bind('mailer', SMTPMailerProvider)
+# m = c.make('mailer')
+# print(isinstance(m, SMTPMailer))
+# quit()
+
+# # We can bind an interface to a implmentation
+# c.bind(MailInterface, SMTPMailerProvider)
+# m = c.make(MailInterface)
+# print(isinstance(m, SMTPMailer))
+# quit()
+
+# # We can bind a class directly without providing instructions on how to init
+# c.bind('direct', SMTPMailer)
+# m = c.make('direct')
+# print(m, isinstance(m, SMTPMailer))
+# quit()
+
+# # We can do zero config resolution
+# m = c.make(ZeroConfig)
+# print(m)
+# quit()
+
+# # We can recusively resolve
+# Weh we ask for a mail interface, we want an instance of SMTP mailer
+c.bind(MailInterface, SMTPMailer)
+# When we need an SMTPMailer, the SMTPMailerProvider must help us build it
+c.bind(SMTPMailer, SMTPMailerProvider)
+# So when we ask for the mailerinterface, we expect a smtpmailer, that the provider helped build
+m = c.make(MailInterface)
+print(isinstance(m, SMTPMailer))
 quit()
+
 
 a = Application({
     'name': 'Python CLI',
--- oak/ContainerTwo.py
+++ oak/ContainerTwo.py
@@ -7,31 +7,67 @@ class ContainerTwo(Singleton):
 
     _bindings = {}
 
-    def bind(self, abstract, concrete, method=None):
+    _instances = {}
 
-        if method == None:
-            self._bindings.update({abstract: concrete})
-        else:
-            self._bindings.update(
-                {abstract: {'provider': concrete, 'method': method}})
+    def bind(self, abstract, concrete, shared=False):
+
+        self._bindings.update({
+            abstract: {
+                'concrete': concrete,
+                'shared': shared,
+            }
+        })
+
+    def instance(self, abstract, instance):
+        self._instances[abstract] = instance
+
+    def singleton(self, abstract, concrete):
+        self.bind(abstract, concrete, True)
 
     def make(self, abstract):
 
+        # 1. If the type has already been resolved as a singleton, just return it
+        if(abstract in self._instances):
+            return self._instances[abstract]
+
+        # 2. Get the registered concrete resolver for this type, otherwise we'll assume we were passed a concretion that we can instantiate
         concrete = self._bindings.get(abstract, abstract)
+
+        if concrete != abstract:
+            concrete = concrete['concrete']
+
+        # 3. If the concrete is either a closure, or we didn't get a resolver, then we'll try to instantiate it.
+        if type(concrete) == types.FunctionType or concrete == abstract:
+            obj = self.build(concrete)
+
+        # 4. Otherwise the concrete must be referencing something else so we'll recursively resolve it until we get either a singleton instance, a closure, or run out of references and will have to try instantiating it.
+        else:
+            obj = self.make(concrete)
+
+        # 5. If the class was registered as a singleton, we will hold the instance so we can always return it.
+        if abstract in self._bindings:
+
+            if self._bindings[abstract].get('shared', False):
+                self._instances[abstract] = obj
+
+        return obj
+
+    def build(self, concrete):
 
         if type(concrete) == types.FunctionType:
             return concrete()
 
         if inspect.isclass(concrete):
-            return concrete()
 
-        if type(concrete) == dict:
-            provider = concrete['provider']
-            method = concrete['method']
-            return getattr(provider(), method)()
+            signature = inspect.signature(concrete)
 
-    def instance(self):
-        pass
+            if signature.empty():
+                return concrete()
 
-    def singleton(self):
-        pass
+            else:
+                print('has a sign')
+                quit()
+
+    def flush(self):
+        self._bindings = {}
+        self._instances = {}
